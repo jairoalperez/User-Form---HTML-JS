ec56be09e081ff577ab919a5a33048616fd9db33
jairoalperez <jairoalberto98@hotmail.com>
2022-12-23 15:08:33 -0500

    Adding Styles

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 function sendInfo() {
     var email = document.getElementById('email').value
     var phone = document.getElementById('phone').value
-    
-    document.getElementById('tdemail').value = email
-    document.getElementById('tdphone').value = phone
 
-    alert('First data recieved!')
+    if (email !== '' && phone !== '') {
+        document.getElementById('mid').classList.remove('hidden')
+        document.getElementById('mid').classList.add('displayed')
+        document.getElementById('first').classList.remove('displayed')
+        document.getElementById('first').classList.add('hidden')
+
+        document.getElementById('tdemail').value = email
+        document.getElementById('tdphone').value = phone
+
+        alert('First data recieved!')
+
+    } else {
+        alert('Insert valid data!')
+    }
+    
 }
 
-function sendFullInfo () {
+function sendFullInfo() {
     var email = document.getElementById('email').value
     var phone = document.getElementById('phone').value
     var title = document.getElementById('title').value
     var name = document.getElementById('name').value
-    var mid = document.getElementById('mid').value
+    var mid = document.getElementById('midd').value
     var last = document.getElementById('lastname').value
     var date = document.getElementById('date').value
     var gender = document.getElementById('gender').value
@@ -26,24 +37,33 @@ function sendFullInfo () {
     var ldate = document.createElement('label')
     var lgender = document.createElement('label')
 
-    var br = document.createElement('br')
+    lemail.innerText = 'E-mail: ' + email
+    lphone.innerText = 'Phone Number: ' + phone
+    luser.innerText = 'User: ' + title + '. ' + name + ' ' + mid + ' ' + last
+    ldate.innerText = 'Birthdate: ' + date
+    lgender.innerText = 'Gender: ' + gender
 
-    lemail.innerText = 'E-mail: '+email
-    lphone.innerText = 'Phone Number: '+phone
-    luser.innerText = 'User: '+title+'. '+name+' '+mid+' '+last
-    ldate.innerText = 'Birthdate: '+date
-    lgender.innerText = 'Gender: '+gender
+    if (email !== '' && phone !== '' && title !== '' && name !== '' && last !== '' && date !== '' && gender !== '') {
+        document.getElementById('mid').classList.remove('displayed')
+        document.getElementById('mid').classList.add('hidden')
+        document.getElementById('last').classList.remove('hidden')
+        document.getElementById('last').classList.add('displayed')
 
-    div.append(lemail)
-    div.innerHTML += '<br><br>'
-    div.append(lphone)
-    div.innerHTML += '<br><br>'
-    div.append(luser)
-    div.innerHTML += '<br><br>'
-    div.append(ldate)
-    div.innerHTML += '<br><br>'
-    div.append(lgender)
+        div.append(lemail)
+        div.innerHTML += '<br><br>'
+        div.append(lphone)
+        div.innerHTML += '<br><br>'
+        div.append(luser)
+        div.innerHTML += '<br><br>'
+        div.append(ldate)
+        div.innerHTML += '<br><br>'
+        div.append(lgender)
 
-    alert('All data recieved!')
+        alert('All data recieved!')
+    } else {
+        alert('Insert valid data!')
+    }
+
+
 
 }
